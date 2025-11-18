b69d32f02c153ac368341fb2db3568998fc409b9
Murali <murali60369@gmail.com>
2025-11-18 18:54:34 +0530

    Added a C program to check greatest of two numbers based on user input

---
+++ PROGRAM TO CHECK GREATEST OF 2 NUM BASED ON USER INPUT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int x,y;
+	printf("ENTER THE VALUE OF x:");
+	scanf("%d",&x);
+	printf("ENTER THE VALUE OF y:");
+	scanf("%d",&y);
+	if(x>y)
+	{
+		printf("THE VALUE a IS GREATER");
+	}
+	else
+	{
+		printf("THE VALUE b IS GREATER ");
+	}
+	
+}
