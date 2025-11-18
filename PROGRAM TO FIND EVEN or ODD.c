4499a1179bc07b2b70795d526fdb455c51bf72bb
Murali <murali60369@gmail.com>
2025-11-18 19:02:49 +0530

    Added a C program to find EVEN or ODD based on user input

---
+++ PROGRAM TO FIND EVEN or ODD.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER A NUMBER:");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("THE VALUE IS EVEN");
+	}
+	else
+	{
+		printf("THE VALUE IS ODD");
+	}
+
+}
