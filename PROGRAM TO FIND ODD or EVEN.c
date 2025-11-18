f994753f34f27a49fdc0848e8a0d052a2ea0eef9
Murali <murali60369@gmail.com>
2025-11-18 19:10:21 +0530

    Added a C program to find ODD or EVEN (2)

---
+++ PROGRAM TO FIND ODD or EVEN.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("ENTER A NUMBER:");
+	scanf("%d",&a);
+	if(a%2==1)
+	{
+		printf("THE VALUE IS ODD");
+	}
+	else
+	{
+		printf("THE VALUE IS EVEN");
+	}
+}
