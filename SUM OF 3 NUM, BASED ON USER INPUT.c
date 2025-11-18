bab71b9f6512bebb001a4e67352e30b0f236b912
Murali <murali60369@gmail.com>
2025-11-18 18:41:06 +0530

    Added a C program to evaluate sum of 3 numbers based on user input

---
+++ SUM OF 3 NUM, BASED ON USER INPUT.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,sum;
+	printf("ENTER THE VALUE OF a:",&a);
+	scanf("%d",&a);
+	printf("ENTER THE VALUE OF b:",&b);
+	scanf("%d",&b);
+	printf("ENTER THE VALUE OF c:",&c);
+	scanf("%d",&c);
+	sum=a+b+c;
+	printf("THE SUM OF THREE NUMBERS IS:%d",sum);
+}
